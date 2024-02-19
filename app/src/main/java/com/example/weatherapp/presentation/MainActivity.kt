079030dfd24de14e9759972143820109f38cf4de
Artem <artem.mamedov5375@gmail.com>
2024-02-19 19:52:23 +0200

    Implement FavoriteContent

--- app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
@@ -5,8 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.arkivanov.decompose.defaultComponentContext
 import com.example.weatherapp.WeatherApp
+import com.example.weatherapp.domain.usecase.ChangeFavoriteStateUseCase
+import com.example.weatherapp.domain.usecase.SearchCityUseCase
 import com.example.weatherapp.presentation.root.DefaultRootComponent
 import com.example.weatherapp.presentation.root.RootContent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
@@ -18,6 +23,7 @@ class MainActivity : ComponentActivity() {
         (applicationContext as WeatherApp).applicationComponent.inject(this)
 
         super.onCreate(savedInstanceState)
+
         setContent {
             RootContent(component = rootComponentFactory.create(defaultComponentContext()))
         }
